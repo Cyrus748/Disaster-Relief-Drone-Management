d6f8502ea3ccfe7e1c6148b0f2ab216c0d0988d0
Aditya Negi <119617732+Cyrus748@users.noreply.github.com>
2024-12-27 10:08:16 +0530

    Update vercel.sh

--- vercel.sh
+++ vercel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+npx shadcn@latest add "https://v0.dev/chat/b/b_0ZsM3yph3Fb?token=eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..TkXATMB6p3BAn-Ug.y3s8KQbhueF6LUlUxrb9QRf2bHgmAhJZic9dO4qrFZfe1W_e13GLaPJUGF8._LG-p91KX35-94hyL3ZHXg"
 npm install
-npm install @radix-ui/react-icons
 
 # Add your actual build command here (e.g., for Next.js)
 npm run build
